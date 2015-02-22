5222c8cd4c47df3a67016a87007f16caf32df7ee
Kristof Kalocsai <kristofkalocsai@gmail.com>
2015-02-22 16:38:55 +0100

    polishing

--- pythonscript/server.py
+++ pythonscript/server.py
@@ -2,18 +2,20 @@ __author__ = 'kristofkalocsai'
 import socket
 
 TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
-BUFFER_SIZE = 50  # Normally 1024, but we want fast response
+TCP_PORT = 5007
+BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(('', TCP_PORT))
 s.listen(1)
 
 conn, addr = s.accept()
 print 'Connection address:', addr
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print "received data:", data
-    conn.send("hello launchpad!\n")  # echo
-conn.close()
+try:
+    while 1:
+        data = conn.recv(BUFFER_SIZE)
+        print "received data:", data
+        conn.send("hello launchpad!\n")     # echo
+except KeyboardInterrupt:
+    conn.close()
